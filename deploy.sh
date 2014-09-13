18ccf0f5d61ce9602c802e636d7e32a972789a5a
James H. Edwards <james.howe.edwards@gmail.com>
2014-09-13 10:50:27 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -117,12 +117,14 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
-# 4. Run Bower install
-if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then  
-  eval $NPM_CMD install bower -g 
-  exitWithMessageOnError "installing bower failed"  
-  ./node_modules/.bin/bower install  
-  exitWithMessageOnError "bower failed"  
+# 4. Install bower packages
+if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install bower
+  exitWithMessageOnError "installing bower failed"
+  ./node_modules/.bin/bower install
+  exitWithMessageOnError "bower failed"
+  cd - > /dev/null
 fi  
 
 ##################################################################################################################################
